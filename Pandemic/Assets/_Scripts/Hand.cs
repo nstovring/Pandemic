24a7b5ea1d60a4ee88c256fa079366b1a4d5dc1a
mkhsor <smakhlouf9@gmail.com>
2015-11-25 14:11:41 +0100

    Hand

--- Pandemic/Assets/_Scripts/Hand.cs
+++ Pandemic/Assets/_Scripts/Hand.cs
@@ -2,8 +2,44 @@
 
 internal class Hand
 {
+    public Card[] hand;
+    GameManager gm;
+    void start()
+    {
+        hand = new Card[7];
+        gm = GameManager.instance;
+    }
+    public void addToHand(Card card)
+    {
+        for (int i = 0; i < hand.Length; i++)
+        {
+            if (hand[i] = null)
+            {
+                hand[i] = card;
+            }
+        }
+
+    }
+
     internal void discard(_cityCard city)
     {
         throw new NotImplementedException();
     }
+    public void discard(Card card)
+    {
+        for (int i = 0; i < hand.Length; i++)
+        {
+            if (card = hand[i])
+            {
+                //gm.playerDiscardStack.add(card);
+                hand[i] = null;
+                break;
+
+            }
+        }
+
+    }
 }
+
+
+
