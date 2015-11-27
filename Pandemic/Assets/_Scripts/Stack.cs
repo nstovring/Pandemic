abaad6a294d0c61e378865479d981553ec9713ec
busskiller <busskiller@gmail.com>
2015-11-27 16:42:48 +0100

    Everything Stacks is more or less done.
    Ask yousif for changes, you noob.

--- Pandemic/Assets/_Scripts/Stack.cs
+++ Pandemic/Assets/_Scripts/Stack.cs
@@ -6,51 +6,43 @@ using System;
 
 public class Stack : MonoBehaviour {
 
-	public _eventCard [] eventCards = new _eventCard [5];
-	public _cityCard [] cityCards = new _cityCard [48];
-	public _infectionCard [] infectionCards = new _infectionCard [48];
-	public _epidemicCard [] epidemicCards = new _epidemicCard [5];
-	public _roleCard [] roleCards = new _roleCard [7];	
-	public enum cardType {CITY, INFECTION, EVENT, EPIDEMIC, ROLE};
-    public enum roleType { MEDIC, DISPATCHER, QURANTINE_SPECIALIST, CONTINGENCY, RESEARCHER, SCIENTIST, OPERATIONS_EXPERT};
-
+	public enum cardType {PLAYER_STACK, INFECTION, ROLE};
 
     public Card [] cards;
 
-
     // Nikolaj - Purely changes for unity
-    public void Initialize(int stackSize, cardType type)
+    public void Initialize(cardType type)
     {
-        cards = new Card[stackSize];
         produceCards(type);
     }
 
-    /*public Stack (int stackSize, cardType type){
-		cards = new Card [stackSize];
-		produceCards (type);
-	}*/
 
     public void produceCards (cardType type){
 		switch (type){
-		case cardType.CITY:	createCityCards();
+		case cardType.PLAYER_STACK:	createPlayerStack();
 		        break;
 		case cardType.INFECTION: createInfectionCards ();
 		        break;
 		case cardType.ROLE:	createRoleCards();
-		        break;
-		case cardType.EVENT:	createEventCards();
-		        break;
-		case cardType.EPIDEMIC:	createEpidemicCards();
 		        break;
 		default:
 			break;
 		}
 	}
 
+    public void shuffleStack()
+    {
+        // Knuth shuffle algorithm
+        for (int i = 0; i < cards.Length; i++)
+        {
+            Card tmp = cards[i];
+            int r = UnityEngine.Random.Range(i, cards.Length);
+            cards[i] = cards[r];
+            cards[r] = tmp;
+        }
+    }
 
-
-
-	public Stack combineStacks (Stack one, Stack two){
+    public Stack combineStacks (Stack one, Stack two){
 
 		Stack newStack = new Stack ();
 		int newStackLength = one.cards.Length + two.cards.Length;
@@ -60,31 +52,6 @@ public class Stack : MonoBehaviour {
 		Array.Copy (two.cards, 0, newStack.cards, one.cards.Length+1, two.cards.Length);
 
 		return newStack;
-	}
-
-    public void ShuffleInfectCards()
-    {
-        // Knuth shuffle algorithm
-        for (int i = 0; i < infectionCards.Length; i++)
-        {
-            _infectionCard tmp = infectionCards[i];
-            int r = UnityEngine.Random.Range(i, infectionCards.Length);
-            infectionCards[i] = infectionCards[r];
-            infectionCards[r] = tmp;
-        }
-    }
-
-    public Stack shuffleStack (ref Stack input){
-
-		// Knuth shuffle algorithm
-		for (int i = 0; i < input.cards.Length; i++ )
-		{
-			Card tmp = input.cards[i];
-			int r = UnityEngine.Random.Range(i, input.cards.Length);
-			input.cards[i] = input.cards[r];
-			input.cards[r] = tmp;
-		}
-		return input;
 	}
 
 	public Stack removeCard (Stack input, String cardName){
@@ -130,25 +97,12 @@ public class Stack : MonoBehaviour {
 		return target;
 	}
 
-	public void createCityCards (){
-		for (int i = 0; i < cityCards.Length; i++){
-			//cityCards [i] = new _cityCard();
-            cityCards[i] = new GameObject().AddComponent<_cityCard>();
-
-        }
-        for (int i = 0; i < cityCards.Length; i++){
-			cityCards [i].name = GameManager.cityNames [i];
-			cityCards [i].cityID = i + 1;
-			//System.out.println(cityCards [i].cityID);
-			//System.out.println(cityCards [i].name);
-			//System.out.println("");
-		}
-		for (int i = 0; i < cityCards.Length; i++){
-			cards[i] = cityCards [i];
-		}
-	}
-
 	public void createInfectionCards (){
+
+        cards = new Card[48];
+
+        _infectionCard[] infectionCards = new _infectionCard[48];
+        
         //Nikolaj - another unity change
 		for (int i = 0; i < infectionCards.Length; i++){
             infectionCards[i] = new GameObject().AddComponent<_infectionCard>();
@@ -166,63 +120,99 @@ public class Stack : MonoBehaviour {
 		}
 	}
 
-	public void createRoleCards () {
-		for (int i = 0; i < roleCards.Length; i++){
-			//roleCards [i] = new _roleCard();
+    public void createRoleCards()
+    {
+        _roleCard[] roleCards = new _roleCard[7];
+        cards = new Card[7];
+
+        for (int i = 0; i < roleCards.Length; i++)
+        {
+            //roleCards [i] = new _roleCard();
             roleCards[i] = new GameObject().AddComponent<_roleCard>();
         }
 
-        roleCards [0].name  = ("MEDIC");
-		roleCards [1].name  = ("DISPATCHER");
-		roleCards [2].name  = ("QURANTINE SPECIALIST");
-		roleCards [3].name  = ("CONTINGENCY");
-		roleCards [4].name  = ("RESEARCHER");
-		roleCards [5].name  = "SCIENTIST";
-		roleCards [6].name  = "OPERATIONS EXPERT";
-		
-		for (int i = 0; i < roleCards.Length; i++){
-			cards[i] = roleCards [i];
-		}
-		
-		for (int i = 0; i < roleCards.Length; i++){
-			//System.out.println(roleCards [i].name);
-			//System.out.println("");
-		}
-		
-	}
+        roleCards[0].name = ("MEDIC");
+        roleCards[1].name = ("DISPATCHER");
+        roleCards[2].name = ("QURANTINE SPECIALIST");
+        roleCards[3].name = ("CONTINGENCY");
+        roleCards[4].name = ("RESEARCHER");
+        roleCards[5].name = "SCIENTIST";
+        roleCards[6].name = "OPERATIONS EXPERT";
 
-	public void createEventCards (){
-		for (int i = 0; i < eventCards.Length; i++){
-			eventCards [i] = new _eventCard();
-		}
-		
-		eventCards [0].name  = "RESILIENT POPULATION";
-		eventCards [1].name  = "ONE QUIET NIGHT";
-		eventCards [2].name  = "FORECAST";
-		eventCards [3].name  = "AIRLIFT";
-		eventCards [4].name  = "GOVERNMENT GRANT";
-		
-		for (int i = 0; i < eventCards.Length; i++){
-			cards[i] = eventCards [i];
-		}
+        for (int i = 0; i < roleCards.Length; i++)
+        {
+            cards[i] = roleCards[i];
+        }
 
-		for (int i = 0; i < eventCards.Length; i++){
-			//System.out.println(eventCards[i].name);
-			//System.out.println("");
-		}
-	}
+        for (int i = 0; i < roleCards.Length; i++)
+        {
+            //System.out.println(roleCards [i].name);
+            //System.out.println("");
+        }
 
-	public void createEpidemicCards (){
-		for (int i = 0; i < epidemicCards.Length; i++){
-			epidemicCards [i] = new _epidemicCard();
-		}
-		for (int i = 0; i <  epidemicCards.Length; i++){
-			epidemicCards [i].name = "Epidemic";
-			//System.out.println(epidemicCards [i].name);
-			//System.out.println("");
-		}
-		for (int i = 0; i < epidemicCards.Length; i++){
-			cards[i] = epidemicCards [i];
-		}
-	}
+    }
+
+    public void createPlayerStack()
+    {
+        _cityCard[] cityCards = new _cityCard[48];
+        _eventCard[] eventCards = new _eventCard[5];
+        cards = new Card[53];
+
+        //Create the city cards
+        for (int i = 0; i < cityCards.Length; i++)
+        {
+            cityCards[i] = new GameObject().AddComponent<_cityCard>();
+        }
+        for (int i = 0; i < cityCards.Length; i++)
+        {
+            cityCards[i].name = GameManager.cityNames[i];
+            cityCards[i].cityID = i + 1;
+        }
+        for (int i = 0; i < cityCards.Length; i++)
+        {
+            cards[i] = cityCards[i];
+        }
+
+        //Create the event cards
+        for (int i = 0; i < eventCards.Length; i++)
+        {
+            eventCards[i] = new _eventCard();
+        }
+        eventCards[0].name = "RESILIENT POPULATION";
+        eventCards[1].name = "ONE QUIET NIGHT";
+        eventCards[2].name = "FORECAST";
+        eventCards[3].name = "AIRLIFT";
+        eventCards[4].name = "GOVERNMENT GRANT";
+
+        for (int i = 0; i < eventCards.Length; i++)
+        {
+            cards[cityCards.Length + i] = eventCards[i];
+        }
+    }
+
+    public void addEpidemicCards()
+    {
+        _epidemicCard[] epidemicCards = new _epidemicCard[5];
+
+        for (int i = 0; i < epidemicCards.Length; i++)
+        {
+            epidemicCards[i] = new _epidemicCard();
+        }
+        for (int i = 0; i < epidemicCards.Length; i++)
+        {
+            epidemicCards[i].name = "Epidemic";
+            //System.out.println(epidemicCards [i].name);
+            //System.out.println("");
+        }
+
+        int tmpSize = cards.Length;
+        Array.Resize(ref cards, cards.Length + 5);
+
+        for (int i = 0; i < epidemicCards.Length; i++) {
+            cards[i+ tmpSize] = epidemicCards[i];
+        }
+
+    }
+
+
 }
