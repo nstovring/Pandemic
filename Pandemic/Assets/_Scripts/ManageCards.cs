23be0e0b734dbdf17cf186b23c30ed8fd8aee541
Jeppe Milling Korsholm <jeppe.korsholm@hotmail.com>
2015-12-01 17:02:50 +0100

    more ui stuff

--- Pandemic/Assets/_Scripts/ManageCards.cs
+++ Pandemic/Assets/_Scripts/ManageCards.cs
@@ -1,25 +1,78 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 using System.Xml.Serialization;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class ManageCards : MonoBehaviour {
+public class ManageCards : MonoBehaviour, IPointerEnterHandler
+{
+
+    private Button button;
+    private Vector3 lerpDown;
+    private Vector3 lerpUp;
 
 
 
-
-    void OnMouseOver()
+    public void OnPointerEnter(PointerEventData eventData)
     {
-        Debug.Log("works");
+
+        StartCoroutine("lerpCard");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("exits card");
+        StartCoroutine("lerpCardDown");
     }
 
 
-	// Use this for initialization
-	void Start () {
-	Debug.Log("starts");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    private IEnumerator lerpCard()
+    {
+        while (transform.position.y <= lerpUp.y - 1)
+        {
+            transform.position = Vector3.Lerp(transform.position,
+                new Vector3(transform.position.x, transform.position.y + 25, transform.position.z), 0.1f);
+            Debug.Log("lerpUp is: "+ lerpUp.y);
+            yield return new WaitForEndOfFrame();
+        }
+        StopCoroutine("lerpCard");
+        
+    }
+
+    /*
+    IEnumerator lerpCardDown()
+    {
+        while (transform.position.y >= lerpUp.y + 1)
+        {
+            transform.position = Vector3.Lerp(transform.position,
+                new Vector3(transform.position.x, transform.position.y - 25, transform.position.z), 0.1f);
+            yield return new WaitForEndOfFrame();
+
+            StopCoroutine("lerpCardDown");
+
+            Debug.Log("coroutine not stopped");
+        }
+
+    }
+    */
+
+    // Use this for initialization
+    void Start()
+    {
+        button = GetComponentInParent<Button>();
+        lerpUp = new Vector3(transform.position.x, transform.position.y + 25, transform.position.z);
+        lerpDown =  new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        Debug.Log(lerpUp.y);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
 }
